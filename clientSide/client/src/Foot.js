6d5e24ec1e9855925eaa43d1b64fcd1708458265
Veronica Giraldo <veronica9893@gmail.com>
2019-06-04 13:31:42 -0600

    footer info

--- clientSide/client/src/Foot.js
+++ clientSide/client/src/Foot.js
@@ -7,19 +7,28 @@ import React from 'react';
 import './foot.css';
 import rp from "./images/headerLogo.png"
 import mvlogo from './images/mvlogosvg.svg'
+import forsale from './images/mv-home-2.png';
 // import {rplogo} from './images/headerLogo.png'
 
 class Foot extends React.Component{
   render(){
     return(
       <footer class="page-footer">
-     
+        
+        <div class="row">
+    <div class="col s12 m6 l3"><p>Icon</p></div>
+    <div class="col s12 m6 l3"><p>realtypath info</p></div>
+    <div class="col s12 m6 l3"><p>realtor info</p></div>
+    <div class="col s12 m6 l3"><p>ugly photo</p></div>
+  </div>
         <div class="row">
           {/* <div class="col s12">MV Realtors</div> */}
-          <div class="col m3 s12">
+          <div class="col m6 s12">
           <img class="realtylogo"src={rp}alt="realtypath"/>
           </div>
-          <div class="col m3 s12">Realtypath LLC.
+          
+          <div class="col m6 s12">
+          <h6>Realtypath LLC.</h6>
           <i class="fas fa-map-marker-alt">  2115 S. Dallin Street
                 Salt Lake City, UT 84109</i>
         
@@ -34,7 +43,7 @@ class Foot extends React.Component{
           <i class="fas fa-at"><a href="mailto: charles.vasquez@realtypath.com" target="_blank" without rel="noopener noreferrer"> charles.vasquez@realtypath.com </a></i>
          
          </div> */}
-      
+        <h6>Contact Me</h6>
           <ul>
           <li><i class="fas fa-user">Misty Vasquez</i></li>
           <li><i class="fas fa-phone"></i> 801.897.6430</li>
@@ -44,17 +53,19 @@ class Foot extends React.Component{
           <li><i class="fas fa-at"><a href="mailto: charles.vasquez@realtypath.com" target="_blank" without rel="noopener noreferrer">charles.vasquez@realtypath.com </a></i></li>
           </ul>
          
-          
+          <div class="col m6 s12 offset-s6">
+      <img className="forsale" src={forsale} alt="forsale"/>
+      </div>
           
       </div>
+    
       
         <div class="footer-copyright">
             <div class="container">
             MV Realtors © 2019 Copyright
             </div>
           </div>
-      
-  
+   
   </footer>
     )
     }
